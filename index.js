571ffeb2ca4d9b878d6c82717854cb218763eaad
Qun Lin <jinasonlin@gmail.com>
2016-04-06 11:23:02 +0800

    1.add transfer(企业付款) 2.update redpack url name

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.Payment = require('./lib/payment').Payment;
 exports.RedPack = require('./lib/redpack').RedPack;
+exports.Transfer = require('./lib/transfer').Transfer;
 exports.middleware = require('./lib/middleware');
